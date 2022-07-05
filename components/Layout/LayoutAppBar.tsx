9d759f3caf2cb5926644f51e9c2f1a747a5357a9
riad <riad@gmail.com>
2022-07-05 09:41:28 +0100

    redo customising theme to NavBar

--- components/Layout/LayoutAppBar.tsx
+++ components/Layout/LayoutAppBar.tsx
@@ -2,7 +2,7 @@ import { AppBar, Switch, ThemeProvider, Toolbar } from '@mui/material';
 import NextLink from 'next/link';
 import { useContext } from 'react';
 
-import darkTheme from '../../utils/Theme';
+// import darkTheme from '../../utils/Theme';
 import ThemeContext from '../../utils/ThemeContext';
 
 function LayoutAppBar() {
@@ -10,38 +10,38 @@ function LayoutAppBar() {
   const handleDarkModeToggle = () => toggleTheme();
 
   return (
-    <ThemeProvider theme={darkTheme}>
-      <AppBar
-        position='static'
-        sx={{
-          backgroundColor: '#203040',
-          fontWeight: 'bold',
-          fontSize: '1rem',
-        }}
-      >
-        <Toolbar>
-          <NextLink href={'/'} passHref>
-            <a>Ecommerce Web App</a>
-          </NextLink>
+    // <ThemeProvider theme={darkTheme}>
+    <AppBar
+      position='static'
+      sx={{
+        backgroundColor: '#203040',
+        fontWeight: 'bold',
+        fontSize: '1rem',
+      }}
+    >
+      <Toolbar>
+        <NextLink href={'/'} passHref>
+          <a>Ecommerce Web App</a>
+        </NextLink>
 
-          <div style={{ flexGrow: 1 }} />
-          <Switch
-            checked={theme === 'dark'}
-            onChange={handleDarkModeToggle}
-            sx={{ marginRight: '3rem' }}
-          >
-            {' '}
-            Theme:{theme}
-          </Switch>
-          <NextLink href={'/cart'} passHref>
-            <a style={{ marginRight: '3rem' }}>Cart</a>
-          </NextLink>
-          <NextLink href={'/login'} passHref>
-            <a style={{ marginRight: '3rem' }}>Login</a>
-          </NextLink>
-        </Toolbar>
-      </AppBar>
-    </ThemeProvider>
+        <div style={{ flexGrow: 1 }} />
+        <Switch
+          checked={theme === 'dark'}
+          onChange={handleDarkModeToggle}
+          sx={{ marginRight: '3rem' }}
+        >
+          {' '}
+          Theme:{theme}
+        </Switch>
+        <NextLink href={'/cart'} passHref>
+          <a style={{ marginRight: '3rem' }}>Cart</a>
+        </NextLink>
+        <NextLink href={'/login'} passHref>
+          <a style={{ marginRight: '3rem' }}>Login</a>
+        </NextLink>
+      </Toolbar>
+    </AppBar>
+    // </ThemeProvider>
   );
 }
 
